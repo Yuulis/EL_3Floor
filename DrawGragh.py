f715be2b6bb206f189b5f1787f7f837420ba1b4c
Yuulis <yuulis2005@gmail.com>
2021-11-02 18:16:06 +0900

    The way image file output updated.

--- DrawGragh.py
+++ DrawGragh.py
@@ -7,27 +7,34 @@ import pandas as pd
 fig = plt.figure(figsize=(10, 5), tight_layout=True)
 plt.subplots_adjust(wspace=0.2, hspace=0.1)
 
+FILE_NAME = "3FloorLv4_result_2021-11-02-15-43-40.csv"
+
 # Input data
-ax_Success = fig.add_subplot(221, title="Success?", xlabel="X pos", ylabel="Z pos")
-Success_Data = pd.read_csv("3FloorLv4_result_2021-10-20-20-47-55.csv", usecols=[0, 1, 2], dtype="float")
+ax_Success = fig.add_subplot(
+    221, title="Success?", xlabel="X pos", ylabel="Z pos")
+Success_Data = pd.read_csv(FILE_NAME, usecols=[0, 1, 2], dtype="float")
 
-ax_ReachStair1 = fig.add_subplot(222, title="Reached stair at Floor1", xlabel="X pos", ylabel="Z pos")
-ReachStair1_Data = pd.read_csv("3FloorLv4_result_2021-10-20-20-47-55.csv", usecols=[0, 1, 3], dtype="float")
+ax_ReachStair1 = fig.add_subplot(
+    222, title="Reached stair at Floor1", xlabel="X pos", ylabel="Z pos")
+ReachStair1_Data = pd.read_csv(FILE_NAME, usecols=[0, 1, 3], dtype="float")
 
-ax_ReachStair2 = fig.add_subplot(223, title="Reached stair at Floor2", xlabel="X pos", ylabel="Z pos")
-ReachStair2_Data = pd.read_csv("3FloorLv4_result_2021-10-20-20-47-55.csv", usecols=[0, 1, 4], dtype="float")
+ax_ReachStair2 = fig.add_subplot(
+    223, title="Reached stair at Floor2", xlabel="X pos", ylabel="Z pos")
+ReachStair2_Data = pd.read_csv(FILE_NAME, usecols=[0, 1, 4], dtype="float")
 
 
 # DataFrame
 df_success = pd.DataFrame(Success_Data, columns=["X", "Z", "Success?"])
-df_ReachStair1 = pd.DataFrame(ReachStair1_Data, columns=["X", "Z", "ReachStair_Floor1"])
-df_ReachStair2 = pd.DataFrame(ReachStair2_Data, columns=["X", "Z", "ReachStair_Floor2"])
+df_ReachStair1 = pd.DataFrame(ReachStair1_Data, columns=[
+                              "X", "Z", "ReachStair_Floor1"])
+df_ReachStair2 = pd.DataFrame(ReachStair2_Data, columns=[
+                              "X", "Z", "ReachStair_Floor2"])
 
 # Success?
-colorList_1 = {-1 : "gray", 0 : "red", 1 : "green"}
-labelName_1 = {-1 : "Error", 0 : "Failure", 1 : "Success"}
+colorList_1 = {-1: "gray", 0: "red", 1: "green"}
+labelName_1 = {-1: "Error", 0: "Failure", 1: "Success"}
 
-for s in set(df_success["Success?"]) : 
+for s in set(df_success["Success?"]):
     df2_success = df_success[df_success["Success?"] == s]
     c = colorList_1[s]
     l = labelName_1[s]
@@ -44,7 +51,8 @@ for r in set(df_ReachStair1["ReachStair_Floor1"]):
     df2_ReachStair1 = df_ReachStair1[df_ReachStair1["ReachStair_Floor1"] == r]
     c = colorList_2[r]
     l = labelName_2[r]
-    ax_ReachStair1.scatter(df2_ReachStair1.X, df2_ReachStair1.Z, s=10, color=c, label=l)
+    ax_ReachStair1.scatter(
+        df2_ReachStair1.X, df2_ReachStair1.Z, s=10, color=c, label=l)
 
 ax_ReachStair1.legend()
 
@@ -54,11 +62,13 @@ for r in set(df_ReachStair2["ReachStair_Floor2"]):
     df2_ReachStair2 = df_ReachStair2[df_ReachStair2["ReachStair_Floor2"] == r]
     c = colorList_2[r]
     l = labelName_2[r]
-    ax_ReachStair2.scatter(df2_ReachStair2.X, df2_ReachStair2.Z, s=10, color=c, label=l)
+    ax_ReachStair2.scatter(
+        df2_ReachStair2.X, df2_ReachStair2.Z, s=10, color=c, label=l)
 
 ax_ReachStair2.legend()
 
 plt.subplots_adjust(left=0.030, right=0.995, top=0.965, bottom=0.040)
 plt.show()
 
-fig.savefig("3FloorLv4_result_2021-10-20-20-47-55.png", facecolor="skyblue")
+IMAGE_NAME = FILE_NAME[0:len(FILE_NAME) - 4] + ".png"
+fig.savefig(IMAGE_NAME, facecolor="skyblue")
